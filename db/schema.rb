54bf9d6b2c0803ed27dce37a7cc98b123d1ddbdd
Lindsey Olson <lindseyolson00@gmail.com>
2017-09-20 13:40:14 -0500

    completed set up of blog app

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912230314) do
+ActiveRecord::Schema.define(version: 20170920175818) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
